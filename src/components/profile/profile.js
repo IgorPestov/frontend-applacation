777c443eaa8d08a1aa24a65ef5dceaa5763d73b2
Igor Pestov <ipestov.exceedteam@gmail.com>
2020-06-10 12:20:32 +0300

    done model component Profile

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -1,60 +1,244 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
-import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
+import PropTypes from "prop-types";
+import {
+  AppBar,
+  CssBaseline,
+  Divider,
+  Drawer,
+  Hidden,
+  IconButton,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Container,
+  Grid,
+  Paper,
+  Toolbar,
+  Typography,
+  Avatar,
+  Button,
+} from "@material-ui/core";
+import { Edit, Menu, Person, AddToPhotos, Folder } from "@material-ui/icons";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
+  grid: { margin: theme.spacing(2) },
   paper: {
-    padding: theme.spacing(2),
-    // margin: "auto",
+    padding: theme.spacing(1),
+    marginTop: 1,
     maxWidht: 500,
   },
   paper1: {
-    padding: theme.spacing(2),
-    height: 200,
-    widht: 60,
+    padding: theme.spacing(1),
+    marginBottom: 3,
+  },
+  paper2: {
+    padding: theme.spacing(1),
+    marginTop: 3,
+    maxWidht: 500,
+    minHeight: 200,
+  },
+  title: {
+    flexGrow: 1,
+  },
+  root: {
+    display: "flex",
+  },
+  drawer: {
+    [theme.breakpoints.up("sm")]: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+  },
+  appBar: {
+    [theme.breakpoints.up("sm")]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth,
+    },
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up("sm")]: {
+      display: "none",
+    },
+  },
+  // necessary for content to be below app bar
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+  large: {
+    width: theme.spacing(15),
+    height: theme.spacing(15),
+  },
+  input: {
+    display: "none",
+  },
+  edit: {
+    flexGrow: 1,
   },
 }));
 
-export default function Profile() {
+export default function Profile(props) {
+  const { window } = props;
   const classes = useStyles();
+  const theme = useTheme();
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <div>
+      <div className={classes.toolbar} />
+      <Divider />
+      <List>
+        {["File", "Profile"].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <Folder /> : <Person />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+    </div>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <Container className={classes.root}>
-      <Paper className={classes.paper}>
-      <Grid  container 
-      spacing={3}
-      style={{ background: "yellow" }} >
-        <Grid container
-        spacing={3}
-         xs = {12}  >  
-          <Grid xs={3}>
-            <Paper className={classes.paper1}>Avatar</Paper>
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            className={classes.menuButton}
+          >
+            <Menu />
+          </IconButton>
+          <Typography variant="h6" className={classes.title} noWrap>
+            Profile
+          </Typography>
+          <Button color="inherit">logout</Button>
+        </Toolbar>
+      </AppBar>
+      <nav className={classes.drawer} aria-label="mailbox folders">
+        <Hidden smUp implementation="css">
+          <Drawer
+            container={container}
+            variant="temporary"
+            anchor={theme.direction === "rtl" ? "right" : "left"}
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden xsDown implementation="css">
+          <Drawer
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            variant="permanent"
+            open
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+      </nav>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Container className={classes.root}>
+          <Grid container className={classes.grid}>
+            <Grid container>
+              <Grid item xs={12}>
+                <Paper className={classes.paper1}>
+                  Avatar
+                  <Avatar
+                    variant="square"
+                    alt="Remy Sharp"
+                    src="/static/images/avatar/1.jpg"
+                    className={classes.large}
+                  />
+                  <input
+                    accept="image/*"
+                    className={classes.input}
+                    id="icon-button-foto"
+                    type="button"
+                  />
+                  <label className={classes.edit} htmlFor="icon-button-foto">
+                    <IconButton
+                      color="primary"
+                      aria-label="upload picture"
+                      component="span"
+                    >
+                      <AddToPhotos />
+                    </IconButton>
+                  </label>
+                  <input
+                    accept="image/*"
+                    className={classes.input}
+                    id="icon-button-edit"
+                    type="button"
+                  />
+                  <label htmlFor="icon-button-edit">
+                    <IconButton
+                      color="primary"
+                      aria-label="upload picture"
+                      component="span"
+                    >
+                      <Edit />
+                    </IconButton>
+                  </label>
+                </Paper>
+              </Grid>
+              <Grid
+                container
+                alignItems="stretch"
+                direction="column"
+                item
+                xs={12}
+              >
+                <Grid>
+                  <Paper className={classes.paper}>firstName</Paper>
+                </Grid>
+                <Grid>
+                  <Paper className={classes.paper}>lastName</Paper>
+                </Grid>
+                <Grid>
+                  <Paper className={classes.paper}>age</Paper>
+                </Grid>
+                <Grid>
+                  <Paper className={classes.paper}>gender</Paper>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item xs={12}>
+              <Paper className={classes.paper2}>aboutYourself</Paper>
+            </Grid>
           </Grid>
-          <Grid xs={9} container style={{ background: "red" }}>
-            <Grid xs={12}>
-              <Paper className={classes.paper}>firstName</Paper>
-            </Grid>
-            <Grid xs={12}>
-              <Paper className={classes.paper}>lastName</Paper>
-            </Grid>
-            <Grid xs={12}>
-              <Paper className={classes.paper}>age</Paper>
-            </Grid>
-            <Grid xs={12}>
-              <Paper className={classes.paper}>gender</Paper>
-            </Grid>
-          </Grid>
-        </Grid>
-        <Grid xs={12}>
-          <Paper className={classes.paper}>aboutYourself</Paper>
-        </Grid>
-      </Grid>
-      </Paper>
-    </Container>
+        </Container>
+      </main>
+    </div>
   );
 }
