bf2d9e09d8a1030a4ac7407ce18b6e7b63dadffc
Igor Pestov <ipestov.exceedteam@gmail.com>
2020-06-10 16:32:54 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import SignIn from "./components/signIn";
 import Profile from "./components/profile";
-
+import SignUp from "./components/signUp"
 function App() {
 
   return (
     <div>
-     <Profile/>
+     <SignUp/>
     </div>
   );
 }
