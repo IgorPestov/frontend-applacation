ac25dc2ac94bfd9588327d2fb99df89844821bc3
Igor Pestov <ipestov.exceedteam@gmail.com>
2020-06-11 15:09:44 +0300

    done route signIn with signUp, erro handling empty fields for signIn

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
+import { Switch, Route, Redirect, withRouter } from "react-router-dom";
 import SignIn from "./components/signIn";
 import Profile from "./components/profile";
-import SignUp from "./components/signUp"
-function App() {
+import SignUp from "./components/signUp";
 
-  return (
-    <div>
+class App extends Component {
+  render() {
+    const { history } = this.props;
 
-     <SignIn/>
-
-    </div>
-  );
+    return (
+      <React.Fragment>
+        <Switch>
+          <Route history={history} path="/signIn" component={SignIn} />
+          <Route history={history} path="/signUp" component={SignUp} />
+          <Route history={history} path="/profile" component={Profile} />
+          <Redirect from="/" to="/signIn" />
+        </Switch>
+      </React.Fragment>
+    );
+  }
 }
-
-export default App;
+export default withRouter(App);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router } from "react-router-dom"
 import App from './App';
+import {createBrowserHistory} from 'history'
 
+const history = createBrowserHistory();
 ReactDOM.render(
-  <React.StrictMode>
+
+  <Router history={history}>
     <App/>
-  </React.StrictMode>,
+  </Router>,
   document.getElementById('root')
 );
 
